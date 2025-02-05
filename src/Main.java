37f24d838b2011bff7c38e39f0fdd8d856366ef1
LAPTOP-NTLPB0PE\Navar <navarrolozano@outlook.com>
2025-02-05 10:22:47 +0100

    Métodos acelerar y mostrar datos

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ import modelo.Coche;
 public class Main {
     public static void main (String[] args){
         //constructor de prueba
-        Coche coche1 = new Coche("Corredor1","Peugeot",5555,500,200, 300,30000);
+        Coche coche1 = new Coche("Corredor1","Peugeot",5555,500,70, 120,3000);
+        Coche coche2 = new Coche("Corredor2","Ibiza",1234,102, 80, 120, 1200);
 
-        coche1.mostrarDatos();
+        //coche1.mostrarDatos();
+        coche1.acelerar();
+        coche2.acelerar();
+
 
 
 
--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -1,5 +1,8 @@
 package modelo;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Coche {
 
     private String nombre, marca;
@@ -17,6 +20,8 @@ public class Coche {
     }
 
     public void mostrarDatos(){
+        //función para imprimir los datos en la consola
+
         System.out.println("nombre: " + nombre);
         System.out.println("marca = " + marca);
         System.out.println("matricula = " + matricula);
@@ -25,4 +30,39 @@ public class Coche {
         System.out.println("velocidad = " + velocidad);
         System.out.println("kmRecorridos = " + kmRecorridos);
     }
-}
+
+    public void acelerar(){
+        /*función para aumentar la velocidad del coche y actualizar los km recorrido, con un parámetro a escribir que no sea inferior a 10, a partir del cv del vehículo */
+
+        Scanner scanner = new Scanner(System.in); //para ingresar datos con el teclado
+        System.out.print("Ingrese la velocidad: "); //ingresar valor a subir la velocidad
+        int incremento = scanner.nextInt(); //al inngresar el dato (como numero entero) se guarda en la variable incremento
+
+        if (incremento < 10) {
+
+            System.out.println("El incremento debe ser al menos 10.");
+            return;
+        }
+            Random random = new Random(); //para generar números aleatorios
+
+        // cv < 100 -> rango mínimo, rango máximo] [0, incremento]
+        // cv > 100 -> rango mínimo, rango máximo [10, incremento]
+            if(cv < 100){
+                //variable random = random.nextInt(rango);
+
+                incremento = random.nextInt(incremento + 1); //se incrementa la velocidad desde 0 hasta el valor añadido
+            }
+            else{
+                incremento = 10 + random.nextInt(incremento - 9); //incrementa la velovidad desde 10 hasta el valor añadido, al restar -9 aseguro que el rango aleatorio empiece desde 10
+            }
+
+            this.velocidad += incremento;
+            this.kmRecorridos += this.velocidad * 0.5; //incremento los km recorridos en un 50% de la velocidad actual
+            System.out.println("La potencia actual del coche es de: " + cv + " CV. Nueva velocidad alcanzada: " + velocidad + " km/h");
+            System.out.println("Kilometros recorridos hasta ahora: " + kmRecorridos + " km");
+        }
+
+
+    }
+
+
